a903a841d4a12e647d5eedcd541949c69f42c509
Peter Sarazin <peter.sarazin@gmail.com>
2025-07-25 01:15:30 -0400

    did some refactoring with cpu_socket, and created some initial data insert scripts.

---
+++ scripts/cpu_socket_inserts.sql
@@ -0,0 +1,4 @@
+
+insert into cpu_socket (cpu_socket_id, pc_component_id, socket_type_id, pin_count) values (1, 1, 1, 40);
+insert into cpu_socket (cpu_socket_id, pc_component_id, socket_type_id, pin_count) values (2, 2, 2, 68);
+insert into cpu_socket (cpu_socket_id, pc_component_id, socket_type_id, pin_count) values (3, 3, 2, 132);
